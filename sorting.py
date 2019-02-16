22c9cfee9dfce2c28f97697d0971b3d9d12ca32e
Kevin Edington <kevin.edington@snapiot.com>
2019-02-15 19:02:39 -0800

    Adding placement sort

--- sorting.py
+++ sorting.py
@@ -15,11 +15,11 @@ def is_sorted(nums):
 	return True
 
 # Bubble Sort 
+# Time Complexity
 # Best Case:	O(n)
 # Worst Case:  	O(n^2)
 # Average Case: O(n*logn)
 # Memory Usage: O(1)
-# Params: nums A list of numbers
 
 def bubble_sort(nums):
 	flag = False
@@ -32,3 +32,20 @@ def bubble_sort(nums):
 				nums[idx+1] = num
 				flag = False
 	return nums
+
+# Placement Sort 
+# Time Complexity 
+# Best Case:	O(n)
+# Worst Case:	O(n^2)
+# Average Case:	O(n*logn)
+# Memory Usage: O(n)
+
+def place_sort(nums):
+	result = []
+	for num in nums:
+		i = 0
+		while len(result) > i and result[i] <  num:
+			i += 1
+		result = result[:i] + [num] + result[i:]
+	return result
+
