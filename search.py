fd7eefd2ad24515b56411079411c19ce002a2158
kedington <kev4725@gmail.com>
2019-02-25 13:26:40 -0800

    Updating search.py

--- search.py
+++ search.py
@@ -1,10 +1,12 @@
 # Searching Algorithms
 
+# Collection of Search Algorithms 
+
 # Basic Search Algorithm 
 # Best Case: 	O(1)
 # Worst Case: 	O(n)
 # Memory: 		O(1)
-# Searches for first apperance of an elm
+# Params: A list of unsorted integers and an element 
 # Return: idx if one exists else None
 
 def search(nums, elm):
@@ -13,8 +15,9 @@ def search(nums, elm):
 			return idx 
 
 # Binary Search 
-# O(logn)
-# O(1)
+# Time:  O(logn)
+# Space: O(1)
+# Params: A list of sorted integers and an element
 # Return True if elm in list else false
 
 def binary_search(nums, elm):
@@ -27,6 +30,12 @@ def binary_search(nums, elm):
 		elif elm < nums[mid]:
 			nums = nums[:mid]
 	return False
+
+# Binary Search idx
+# Time:  O(logn)
+# Space: O(1)
+# Params: A list of sorted integers and an element
+# Return idx if element exists else None
 
 def binary_search_idx(nums, elm):
 	idx = 0
@@ -41,3 +50,32 @@ def binary_search_idx(nums, elm):
 			nums = nums[:mid]
 	return None
 
+# Nth Element
+# Time:  O(n) 
+# Space: O(n)
+# Params: A list of unique unsorted integers and an idx
+# Return: the Nth elment in an array
+
+def nth_element(nums, n):
+	if not nums:
+		return None
+	if not 0 < n < len(nums):
+		return None
+	
+	while True:
+		elm = nums[0]
+		left_side = []
+		right_side = []
+		for num in nums:
+			if num < elm:
+				left_side.append(num)
+			elif num > elm:
+				right_side.append(num)
+		if len(left_side) == n:
+			return nums[n]
+		elif len(left_side) > n:
+			nums = left_side	
+		elif len(left_side) < n:
+			nums = right_side	
+			n -= len(left_side) +=1
+	
