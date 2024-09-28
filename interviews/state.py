5c97fba57b75ed0d600701ef913fdc106b299760
kedington <kevin@peopledatalabs.com>
2024-09-28 14:09:13 -0700

    state question

---
+++ interviews/state.py
@@ -0,0 +1,14 @@
+class State:
+	def __init__(self, state)
+		self.state = state
+
+	def L(self):
+		# Set smallest 0 to 1 or do nothing
+
+	def C(self, index):
+		# Set index to 0 regadless of value
+
+	
+def run_operations(state, operations):
+	state_obj = State(state)
+
